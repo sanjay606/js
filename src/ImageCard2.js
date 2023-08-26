252dc52c9520bc77bb593f01d0d46f0c71091724
sanjay <sanjaylashkarmaa1baba1@gmail.com>
2023-08-26 22:57:09 +0530

    react.js

--- src/ImageCard2.js
+++ src/ImageCard2.js
@@ -1,17 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
+
+
+
 function ImageCard2({title,imgsrc,desc}) {
+  const [Count,SetCount] = useState(0);
+  const HandleClick=()=>{
+    
+    SetCount(Count+1);
+    console.log('button clicked',Count);
+  }
+ 
+  const SandleClick=()=>{
+    if(Count>0)
+    SetCount(Count-1);
+    console.log('button clicked',Count);
+    
+  }
+
   return (
-    <Card style={{ width: '18rem' }}>
+    <Card className='bo' style={{ width: '18rem' }}>
     <Card.Img className ="imagewithme" variant="top" src={imgsrc} />
     <Card.Body  >
       <Card.Title>{title}</Card.Title>
       <Card.Text>
        {desc}
       </Card.Text>
-      <Button variant="primary">Go somewhere</Button>
+      <Button onClick={HandleClick} variant="primary">+</Button> 
+      <Button className='subs' onClick={SandleClick} variant="primary">-</Button>{Count}
+      
     </Card.Body>
   </Card>
   )
--- src/datas.js
+++ src/datas.js
@@ -3,38 +3,62 @@ const datas=
     {
       "albumId": 1,
       "id": 1,
-      "title": "accusamus beatae ad facilis cum similique qui sunt",
-      "url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPRY7IhSMPmj5J4C1Hmfj6a1b28gzAMY_oVA&usqp=CAU",
+      "title": "Fire-Boltt Phoenix Smart Watch with Bluetooth Calling 1.3",
+      "url": "https://m.media-amazon.com/images/I/61y2VVWcGBL._AC_UL600_FMwebp_QL65_.jpg",
       "thumbnailUrl": "https://via.placeholder.com/150/92c952"
   },
   {
       "albumId": 1,
       "id": 2,
-      "title": "reprehenderit est deserunt velit ipsam",
-      "url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPRY7IhSMPmj5J4C1Hmfj6a1b28gzAMY_oVA&usqp=CAU",
+      "title": "Noise Twist Bluetooth Calling Smart Watch with 1.38",
+      "url": "https://m.media-amazon.com/images/I/61yhPPcmesL._AC_UL600_FMwebp_QL65_.jpg",
       "thumbnailUrl": "https://via.placeholder.com/150/771796"
   },
   {
       "albumId": 1,
       "id": 3,
-      "title": "officia porro iure quia iusto qui ipsa ut modi",
-      "url": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPRY7IhSMPmj5J4C1Hmfj6a1b28gzAMY_oVA&usqp=CAU",
+      "title": "officia porro iure quia iusto qui ipsa ut Fire-Boltt Phoenix Smart ",
+      "url": "https://m.media-amazon.com/images/I/61xXm6kS-hL._AC_UL600_FMwebp_QL65_.jpg",
       "thumbnailUrl": "https://via.placeholder.com/150/24f355"
   },
   {
       "albumId": 1,
       "id": 4,
-      "title": "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
-      "url": "https://via.placeholder.com/600/d32776",
+      "title": "Fire-Boltt Phoenix Smart Watch with Bluetooth Calling 1.3",
+      "url": "https://m.media-amazon.com/images/I/61xXm6kS-hL._AC_UL600_FMwebp_QL65_.jpg",
       "thumbnailUrl": "https://via.placeholder.com/150/d32776"
   },
   {
       "albumId": 1,
-      "id": 5,
-      "title": "natus nisi omnis corporis facere molestiae rerum in",
-      "url": "https://via.placeholder.com/600/f66b97",
-      "thumbnailUrl": "https://via.placeholder.com/150/f66b97"
-  }
+      "id": 3,
+      "title": "officia porro iure quia iusto qui ipsa ut Fire-Boltt Phoenix Smart ",
+      "url": "https://m.media-amazon.com/images/I/61xXm6kS-hL._AC_UL600_FMwebp_QL65_.jpg",
+      "thumbnailUrl": "https://via.placeholder.com/150/24f355"
+  },
+  {
+      "albumId": 1,
+      "id": 4,
+      "title": "Fire-Boltt Phoenix Smart Watch with Bluetooth Calling 1.3",
+      "url": "https://m.media-amazon.com/images/I/61xXm6kS-hL._AC_UL600_FMwebp_QL65_.jpg",
+      "thumbnailUrl": "https://via.placeholder.com/150/d32776"
+  },
+  {
+    "albumId": 1,
+    "id": 1,
+    "title": "Fire-Boltt Phoenix Smart Watch with Bluetooth Calling 1.3",
+    "url": "https://m.media-amazon.com/images/I/61y2VVWcGBL._AC_UL600_FMwebp_QL65_.jpg",
+    "thumbnailUrl": "https://via.placeholder.com/150/92c952"
+},
+{
+    "albumId": 1,
+    "id": 2,
+    "title": "Noise Twist Bluetooth Calling Smart Watch with 1.38",
+    "url": "https://m.media-amazon.com/images/I/61yhPPcmesL._AC_UL600_FMwebp_QL65_.jpg",
+    "thumbnailUrl": "https://via.placeholder.com/150/771796"
+}
+
+
+  
 ];
 export default datas;
       
