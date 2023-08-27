58755f0f3a72aa7119d6c21b18f33d1453d9b978
sanjay <sanjaylashkarmaa1baba1@gmail.com>
2023-08-27 14:08:52 +0530

    returnreact

--- src/App.js
+++ src/App.js
@@ -6,47 +6,60 @@ import './ImageCard2.css';
 // import datas from './datas';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import ChildComponent from './ChildComponent';
 
-const nCard=(val)=>{
-  return (
-    <ImageCard2
-    key={val.id}
-    imgsrc={val.image}
-    title={val.title}
-    desc={val.price}
-    />
-  );
-  }
+// const nCard=(val)=>{
+//   return (
+//     <ImageCard2
+//     key={val.id}
+//     imgsrc={val.image}
+//     title={val.title}
+//     desc={val.price}
+//     />
+//   );
+//   }
 
 
 function App(){
-  const [data,SetData] = useState([]);
-  useEffect(() =>{
-    const fetchData = async () =>{
-      try{
-        const response = await fetch('https://fakestoreapi.com/products');
-        const data = await response.json();
-        SetData(data);
-      }
+  // const [data,SetData] = useState([]);
+  // useEffect(() =>{
+  //   const fetchData = async () =>{
+  //     try{
+  //       const response = await fetch('https://fakestoreapi.com/products');
+  //       const data = await response.json();
+  //       SetData(data);
+  //     }
       
-        catch(error)
-        {
-          console.log('Error Occured',error);
-        }
+  //       catch(error)
+  //       {
+  //         console.log('Error Occured',error);
+  //       }
         
         
-      };
-      fetchData();
-    },[]);
+  //     };
+  //     fetchData();
+  //   },[]);
 
+  const [DataFromChild,SetDataFromChild] = useState(null);
+  const HandleChild=(children)=>{
+    SetDataFromChild(children);
+  }
   return (
-    <div className='container'>
-    <NavbarComp/>
-    <h1 className='text-center'>Card</h1>
-      <div className='row'>
-           {data.map((nCard))}
-      </div>
-    </div>
+    // <div className='container'>
+    // <NavbarComp/>
+    // <h1 className='text-center'>Card</h1>
+    //   <div className='row'>
+    //        {data.map((nCard))}
+    //   </div>
+    // </div>
+    <>
+      
+    <ChildComponent 
+      OnDataFromChild={HandleChild}
+      />
+      {DataFromChild && <p> {DataFromChild} </p>}
+
+    </>
   );
 }
 
